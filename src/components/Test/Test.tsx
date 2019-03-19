5cfa14e74a131198db2db6177e4db2810e4ad666
jang2162 <jang2162_@naver.com>
2019-03-19 17:44:03 +0900

    containers 구조 components로 통합

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,7 +1,24 @@
-import TestContainer, {TestPropType} from "@/containers/TestContainer";
+import {TestActions} from "@/modules/TestModule";
+import {RootState} from "@/store";
 import React from "react";
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+
+const mapStateToProps = (state: RootState) => ({
+    name: state.test.name,
+    value: state.test.counter.value
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+    ...bindActionCreators(TestActions, dispatch)
+});
+
+type TestPropType =
+    ReturnType<typeof mapStateToProps> &
+    ReturnType<typeof mapDispatchToProps>
 
 interface TestProp {test: string}
+
 const Test = (props: TestPropType & TestProp) => {
     const click = () => props.changeNameAndDelayedAdd({addNum: 5, name: "NEW TEST"});
     return (
@@ -14,4 +31,4 @@ const Test = (props: TestPropType & TestProp) => {
     )
 };
 
-export default TestContainer(Test);
+export default connect(mapStateToProps, mapDispatchToProps)(Test);
