3d14ed838bdf55c5bb725cdf5b8cbcf578bb0978
jbh <jang2162_@naver.com>
2019-04-07 19:54:46 +0900

    달력 뷰 추가

--- src/components/MonthlyView/MonthlyView.tsx
+++ src/components/MonthlyView/MonthlyView.tsx
@@ -15,20 +15,25 @@ const MonthlyView = (props: {curDate: number}) => {
         return (
             <Row key={i}>
                 {
+                    // 이전달
                     i === 0 && prevMonthDayOfWeek !== 6 &&
                     Array(prevMonthDayOfWeek+1).fill(0)
                         .map((ee,j)=> (<Item notCurrent key={`before${j}`}><span>{prevMonth.date()-j}</span></Item>)).reverse()
                 }
 
                 {
+                    // 현재
                     Array(7).fill(0)
-                    .map((ee, j)=> {
-                        const day = j + i * 7 - (prevMonthDayOfWeek === 6 ? -1 : prevMonthDayOfWeek);
-                        return (i === rowNum - 1 ?  nextMonthDayOfWeek === 0 || j < nextMonthDayOfWeek : day > 0) ? (<Item key={day}><span>{day}</span></Item>) : null;
-                    })
+                        .map((ee, j)=> {
+                            const day = j + i * 7 - (prevMonthDayOfWeek === 6 ? -1 : prevMonthDayOfWeek);
+                            return (i === rowNum - 1 ?  nextMonthDayOfWeek === 0 || j < nextMonthDayOfWeek : day > 0) ? day : 0;
+                        })
+                        .filter(j => j !== 0)
+                        .map(day => (<Item key={day}><span>{day}</span></Item>))
                 }
 
                 {
+                    // 다음달
                     i === rowNum - 1 && nextMonthDayOfWeek !== 0 &&
                     Array(7-nextMonth.day()).fill(0)
                         .map((ee,j)=> (<Item notCurrent key={`next${j}`}><span>{j+1}</span></Item>))
